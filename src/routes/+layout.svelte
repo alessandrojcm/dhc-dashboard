9f444b43b937f5a127c67348cbe3e38fbac22069
Alessandro Cuppari <me@alessandrojcm.dev>
2024-11-17 19:40:51 +0000

    feat: rbac for logging in & roles

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,22 +1,28 @@
 <script lang="ts">
-	import Header from './Header.svelte';
 	import '../app.css';
+	import type { LayoutData } from './$types';
+	import { type Snippet } from 'svelte';
+	import { invalidate } from '$app/navigation';
 
-	let { children } = $props();
+	let { children, data }: {children: Snippet, data: LayoutData} = $props();
+	let session = $derived(data.session)
+	let supabase = $derived(data.supabase)
+
+	$effect(() => {
+		const { data } = supabase.auth.onAuthStateChange((_, newSession) => {
+			if (newSession?.expires_at !== session?.expires_at) {
+				invalidate('supabase:auth');
+			}
+		});
+
+		return () => data.subscription.unsubscribe();
+	})
 </script>
 
 <div class="app">
-	<Header />
-
 	<main>
 		{@render children()}
 	</main>
-
-	<footer>
-		<p>
-			visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to learn about SvelteKit
-		</p>
-	</footer>
 </div>
 
 <style>
@@ -35,23 +41,5 @@
 		max-width: 64rem;
 		margin: 0 auto;
 		box-sizing: border-box;
-	}
-
-	footer {
-		display: flex;
-		flex-direction: column;
-		justify-content: center;
-		align-items: center;
-		padding: 12px;
-	}
-
-	footer a {
-		font-weight: bold;
-	}
-
-	@media (min-width: 480px) {
-		footer {
-			padding: 12px 0;
-		}
 	}
 </style>
---
+++ src/routes/auth/+page.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+	import { page } from '$app/stores';
+	const hash = $page.url.hash.split('#')[1] as string;
+	const errorMessage = new URLSearchParams(hash).get('error_description');
+
+	export const prerender = true;
+</script>
+
+<form method="POST">
+	{#if errorMessage}
+		<p>{errorMessage}</p>
+	{/if}
+	<button type="submit">Login with discord</button>
+</form>
