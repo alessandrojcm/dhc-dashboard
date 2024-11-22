a42985bcf43476639bec3be318a42b61bbcd0a49
alessandrojcm <me@alessandrojcm.dev>
2024-11-22 12:13:14 +0000

    ``` fix(layout): adjust dashboard width and enhance waitlist table
    Updated dashboard layout to account for sidebar width. Enhanced waitlist table with dynamic cell rendering and improved data handling for a better user interface and experience.
    ```

--- src/routes/dashboard/+layout.svelte
+++ src/routes/dashboard/+layout.svelte
@@ -78,7 +78,7 @@
 		flex: 1;
 		display: flex;
 		flex-direction: column;
-		width: 100%;
+		width: calc(100vw - var(--sidebar-width));
 		margin: 0 auto;
 		box-sizing: border-box;
 		padding-bottom: 1rem;
