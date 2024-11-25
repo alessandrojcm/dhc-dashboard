a0c7ba0956d16e36aa92e522ebb537853f5d5f7d
Alessandro Cuppari <me@alessandrojcm.dev>
2024-11-25 14:14:43 +0000

    refactor: streamline dashboard and waitlist components
    Updated the dashboard to use createQuery and removed QueryClientProvider wrapping. Introduced tabs for better navigation. Renamed and reorganized components related to the beginners' workshop waitlist.

---
+++ src/routes/dashboard/beginners-workshop/+page.svelte
@@ -0,0 +1,22 @@
+<script lang="ts">
+	import WaitlistTable from './waitlist-table.svelte';
+	import * as Tabs from '$lib/components/ui/tabs/index.js';
+	import { page } from '$app/stores';
+	import { goto } from '$app/navigation';
+
+	const { data } = $props();
+	const supabase = data.supabase;
+	let value = $state('dashboard')
+</script>
+<Tabs.Root bind:value  class="p-2 min-h-96 mr-2">
+	<Tabs.List>
+		<Tabs.Trigger value="dashboard">Dashboard</Tabs.Trigger>
+		<Tabs.Trigger value="waitlist">Waitlist</Tabs.Trigger>
+	</Tabs.List>
+	<Tabs.Content value="dashboard">
+		Dashboard
+	</Tabs.Content>
+	<Tabs.Content value="waitlist">
+		<WaitlistTable {supabase} />
+	</Tabs.Content>
+</Tabs.Root>
--- src/routes/dashboard/beginners-workshop/waitlist-table.svelte
+++ src/routes/dashboard/beginners-workshop/waitlist-table.svelte
@@ -1,9 +1,5 @@
 <script lang="ts">
-	import {
-		createMutation,
-		createQuery,
-		keepPreviousData,
-	} from '@tanstack/svelte-query';
+	import { createMutation, createQuery, keepPreviousData } from '@tanstack/svelte-query';
 	import {
 		createSvelteTable,
 		FlexRender,
@@ -20,7 +16,7 @@
 	} from '@tanstack/table-core';
 	import { page } from '$app/stores';
 	import { goto } from '$app/navigation';
-	import type { Tables } from '$database';
+	import type { Database, Tables } from '$database';
 	import * as Select from '$lib/components/ui/select';
 	import * as Table from '$lib/components/ui/table/index.js';
 	import * as Pagination from '$lib/components/ui/pagination/index.js';
@@ -28,18 +24,20 @@
 	import SortHeader from '$lib/components/ui/table/sort-header.svelte';
 	import dayjs from 'dayjs';
 	import { createRawSnippet } from 'svelte';
-	import type { PostgrestResponse, QueryData } from '@supabase/supabase-js';
+	import type { QueryData, SupabaseClient } from '@supabase/supabase-js';
 	import type { FetchAndCountResult, MutationPayload } from '$lib/types';
 	import { Input } from '$lib/components/ui/input';
 	import { Button } from '$lib/components/ui/button';
 	import { Cross2 } from 'svelte-radix';
 	import ActionButtons from './actions-buttons.svelte';
+
 	const columns =
 		'current_position,full_name,email,phone_number,status,age,initial_registration_date,last_contacted,medical_conditions,admin_notes';
 
 	let pageSizeOptions = [10, 25, 50, 100];
-	const { data } = $props();
-	const supabase = data.supabase;
+
+	const { supabase }: { supabase: SupabaseClient<Database> } = $props();
+
 	const currentPage = $derived(Number($page.url.searchParams.get('page')) || 0);
 	const pageSize = $derived(Number($page.url.searchParams.get('pageSize')) || 10);
 	const searchQuery = $derived($page.url.searchParams.get('q') || '');
@@ -149,7 +147,8 @@
 				cell: ({ getValue }) => {
 					return renderSnippet(
 						createRawSnippet((value) => ({
-							render: () => `<div class="w-[100px] md:w-[120px] whitespace-break-spaces break-words">${value()}</div>`
+							render: () =>
+								`<div class="w-[100px] md:w-[120px] whitespace-break-spaces break-words">${value()}</div>`
 						})),
 						getValue()
 					);
@@ -285,110 +284,108 @@
 	const table = createSvelteTable(tableOptions);
 </script>
 
-<h1 class="prose prose-h1 text-xl ml-2">Beginners Workshop Waitlist</h1>
-<div class="rounded-md border min-h-96 mx-2 md:m-10 p-2">
-	<div class="flex w-full max-w-sm items-center space-x-2 mb-2">
-		<Input
-			value={searchQuery}
-			onchange={(t) => onSearchChange(t.target?.value)}
-			placeholder="Search for a person"
-			class="max-w-md"
-		/>
+<h2 class="prose prose-h2 text-lg mb-2 ml-2">Waitlist</h2>
+<div class="flex w-full max-w-sm items-center space-x-2 mb-2 p-2">
+	<Input
+		value={searchQuery}
+		onchange={(t) => onSearchChange(t.target?.value)}
+		placeholder="Search for a person"
+		class="max-w-md"
+	/>
 
-		<Button variant="ghost" type="button" onclick={() => onSearchChange('')}>
-			<Cross2 />
-		</Button>
-	</div>
-	<div class="overflow-x-auto overflow-y-auto h-[75vh]">
-		<Table.Root class="w-full">
-			<Table.Header class="sticky top-0 z-10 bg-white">
-				{#each table.getHeaderGroups() as headerGroup (headerGroup.id)}
-					{#each headerGroup.headers as header (header.id)}
-						<Table.Head class="text-black prose prose-p text-xs md:text-sm font-medium p-2">
-							<FlexRender content={header.column.columnDef.header} context={header.getContext()} />
-						</Table.Head>
+	<Button variant="ghost" type="button" onclick={() => onSearchChange('')}>
+		<Cross2 />
+	</Button>
+</div>
+<div class="overflow-x-auto overflow-y-auto h-[75vh]">
+	<Table.Root class="w-full">
+		<Table.Header class="sticky top-0 z-10 bg-white">
+			{#each table.getHeaderGroups() as headerGroup (headerGroup.id)}
+				{#each headerGroup.headers as header (header.id)}
+					<Table.Head class="text-black prose prose-p text-xs md:text-sm font-medium p-2">
+						<FlexRender content={header.column.columnDef.header} context={header.getContext()} />
+					</Table.Head>
+				{/each}
+			{/each}
+		</Table.Header>
+		<Table.Body>
+			{#each table.getRowModel().rows as row (row.id)}
+				<Table.Row>
+					{#each row.getVisibleCells() as cell (cell.id)}
+						<Table.Cell
+							class="whitespace-normal md:whitespace-nowrap py-2 md:py-4 px-2 md:px-3 text-xs md:text-sm prose prose-p"
+						>
+							<FlexRender content={cell.column.columnDef.cell} context={cell.getContext()} />
+						</Table.Cell>
 					{/each}
-				{/each}
-			</Table.Header>
-			<Table.Body>
-				{#each table.getRowModel().rows as row (row.id)}
-					<Table.Row>
-						{#each row.getVisibleCells() as cell (cell.id)}
-							<Table.Cell
-								class="whitespace-normal md:whitespace-nowrap py-2 md:py-4 px-2 md:px-3 text-xs md:text-sm prose prose-p"
-							>
-								<FlexRender content={cell.column.columnDef.cell} context={cell.getContext()} />
-							</Table.Cell>
-						{/each}
-					</Table.Row>
-				{/each}
-			</Table.Body>
-			<Table.Footer class="sticky bottom-0 z-10 bg-white">
-				{#each table.getFooterGroups() as footerGroup}
-					<Table.Row>
-						{#each footerGroup.headers as header}
-							<Table.Cell>
-								{#if !header.isPlaceholder}
-									<FlexRender
-										content={header.column.columnDef.footer}
-										context={header.getContext()}
-									/>
-								{/if}
-							</Table.Cell>
-						{/each}
-					</Table.Row>
-				{/each}
-			</Table.Footer>
-		</Table.Root>
-	</div>
-	<div class="flex items-start justify-end space-x-2 py-4 mr-4">
-		<div class="flex items-center gap-2 mr-auto ml-4">
-			<p class="prose">Elements per page</p>
-			<Select.Root
-				type="single"
-				value={pageSize.toString()}
-				onValueChange={(value) => onPaginationChange({ pageSize: Number(value) })}
-			>
-				<Select.Trigger class="w-16">{pageSize}</Select.Trigger>
-				<Select.Content>
-					{#each pageSizeOptions as pageSizeOption}
-						<Select.Item value={pageSizeOption.toString()}>
-							{pageSizeOption}
-						</Select.Item>
+				</Table.Row>
+			{/each}
+		</Table.Body>
+		<Table.Footer class="sticky bottom-0 z-10 bg-white">
+			{#each table.getFooterGroups() as footerGroup}
+				<Table.Row>
+					{#each footerGroup.headers as header}
+						<Table.Cell>
+							{#if !header.isPlaceholder}
+								<FlexRender
+									content={header.column.columnDef.footer}
+									context={header.getContext()}
+								/>
+							{/if}
+						</Table.Cell>
 					{/each}
-				</Select.Content>
-			</Select.Root>
-		</div>
-		<Pagination.Root
-			count={waitlistQuery?.data?.count ?? 0}
-			perPage={pageSize}
-			page={currentPage + 1}
-			onPageChange={(page) => table.setPageIndex(page - 1)}
-			class="m-0 w-auto"
+				</Table.Row>
+			{/each}
+		</Table.Footer>
+	</Table.Root>
+</div>
+<div class="flex items-start justify-end space-x-2 py-4 mr-4">
+	<div class="flex items-center gap-2 mr-auto ml-4">
+		<p class="prose">Elements per page</p>
+		<Select.Root
+			type="single"
+			value={pageSize.toString()}
+			onValueChange={(value) => onPaginationChange({ pageSize: Number(value) })}
 		>
-			{#snippet children({ pages, currentPage })}
-				<Pagination.Content>
-					<Pagination.Item>
-						<Pagination.PrevButton />
-					</Pagination.Item>
-					{#each pages as page (page.key)}
-						{#if page.type === 'ellipsis'}
-							<Pagination.Item>
-								<Pagination.Ellipsis />
-							</Pagination.Item>
-						{:else}
-							<Pagination.Item>
-								<Pagination.Link {page} isActive={currentPage === page.value}>
-									{page.value}
-								</Pagination.Link>
-							</Pagination.Item>
-						{/if}
-					{/each}
-					<Pagination.Item>
-						<Pagination.NextButton />
-					</Pagination.Item>
-				</Pagination.Content>
-			{/snippet}
-		</Pagination.Root>
+			<Select.Trigger class="w-16">{pageSize}</Select.Trigger>
+			<Select.Content>
+				{#each pageSizeOptions as pageSizeOption}
+					<Select.Item value={pageSizeOption.toString()}>
+						{pageSizeOption}
+					</Select.Item>
+				{/each}
+			</Select.Content>
+		</Select.Root>
 	</div>
+	<Pagination.Root
+		count={waitlistQuery?.data?.count ?? 0}
+		perPage={pageSize}
+		page={currentPage + 1}
+		onPageChange={(page) => table.setPageIndex(page - 1)}
+		class="m-0 w-auto"
+	>
+		{#snippet children({ pages, currentPage })}
+			<Pagination.Content>
+				<Pagination.Item>
+					<Pagination.PrevButton />
+				</Pagination.Item>
+				{#each pages as page (page.key)}
+					{#if page.type === 'ellipsis'}
+						<Pagination.Item>
+							<Pagination.Ellipsis />
+						</Pagination.Item>
+					{:else}
+						<Pagination.Item>
+							<Pagination.Link {page} isActive={currentPage === page.value}>
+								{page.value}
+							</Pagination.Link>
+						</Pagination.Item>
+					{/if}
+				{/each}
+				<Pagination.Item>
+					<Pagination.NextButton />
+				</Pagination.Item>
+			</Pagination.Content>
+		{/snippet}
+	</Pagination.Root>
 </div>
