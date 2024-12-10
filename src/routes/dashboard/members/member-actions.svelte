8a2e51b45835ae763670f1e909594e73f4642903
alessandrojcm <me@alessandrojcm.dev>
2024-12-10 21:25:57 +0000

    feat: emergency contact sidebar

--- src/routes/dashboard/members/member-actions.svelte
+++ src/routes/dashboard/members/member-actions.svelte
@@ -1,13 +1,14 @@
 <script lang="ts">
 	import * as Tooltip from '$lib/components/ui/tooltip/index.js';
 	import { Button } from '$lib/components/ui/button';
-	import { Eye } from 'lucide-svelte';
-	import { goto } from '$app/navigation';
+	import { Ambulance, Edit } from 'lucide-svelte';
 
 	type Props = {
 		memberId: string;
+		userId: string;
+		setSelectedUserId: (id: string) => void;
 	};
-	const { memberId }: Props = $props();
+	let { memberId, userId, setSelectedUserId }: Props = $props();
 </script>
 
 <Tooltip.Root>
@@ -15,11 +16,24 @@
 		<Button
 			variant="ghost"
 			size="icon"
-			aria-label="View member details"
-			onclick={() => goto(`/dashboard/members/${memberId}`)}
+			aria-label="View member Emergency Contact"
+			onclick={() => setSelectedUserId(userId)}
 		>
-			<Eye class="h-4 w-4" />
+			<Ambulance class="h-4 w-4" />
 		</Button>
 	</Tooltip.Trigger>
-	<Tooltip.Content>View member details</Tooltip.Content>
+	<Tooltip.Content>View member emergency contact</Tooltip.Content>
+</Tooltip.Root>
+<Tooltip.Root>
+	<Tooltip.Trigger>
+		<Button
+			variant="ghost"
+			size="icon"
+			aria-label="Edit member details"
+			href={`/dashboard/members/${memberId}`}
+		>
+			<Edit class="h-4 w-4" />
+		</Button>
+	</Tooltip.Trigger>
+	<Tooltip.Content>Edit member details</Tooltip.Content>
 </Tooltip.Root>
