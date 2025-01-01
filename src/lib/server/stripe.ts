3a3a452fc3b3c313b82c92096ad671916cbe9985
alessandrojcm <me@alessandrojcm.dev>
2025-01-01 14:08:41 +0000

    feat: add webhook for the invoice.paid event

--- src/lib/server/stripe.ts
+++ src/lib/server/stripe.ts
@@ -2,5 +2,5 @@ import stripe from "stripe";
 import { env } from '$env/dynamic/private';
 
 export const stripeClient = new stripe(env.STRIPE_SECRET_KEY!, {
-    apiVersion: '2024-11-20.acacia',
+    apiVersion: '2024-12-18.acacia',
 });
