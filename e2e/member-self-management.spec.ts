b21eef3149bfed7347b691df3df1f1cf4cd5e2ae
Alessandro Cuppari <me@alessandrojcm.dev>
2025-01-06 18:27:31 +0000

    feat: add link to manage payment settings

--- e2e/member-self-management.spec.ts
+++ e2e/member-self-management.spec.ts
@@ -1,12 +1,13 @@
 import { test, expect } from '@playwright/test';
 import { createMember } from './setupFunctions';
 import { loginAsUser } from './supabaseLogin';
-
-
+// TODO: add test for settings mutations
+// TODO: update stripe details when settings are updated (only phone & name)
+// TODO: add webhook for when a subscription is updated/cancelled/paid
 test.describe('Member Self-Management', () => {
 	let testData: Awaited<ReturnType<typeof createMember>>;
 	test.beforeAll(async () => {
-		testData = await createMember({ email: 'test@test.com' });
+		testData = await createMember({ email: 'test@test.com', createSubscription: true });
 	});
 	test.beforeEach(async ({ context }) => {
 		await loginAsUser(context, 'test@test.com');
@@ -30,6 +31,12 @@ test.describe('Member Self-Management', () => {
 		await expect(page.getByLabel(/first name/i)).toHaveValue('Updated name');
 	});
 
+	test('it should show manage subscription button', async ({ page }) => {
+		await page.goto('/dashboard');
+		await page.getByText(/manage payment settings/i).click();
+		await expect(page).toHaveURL(/billing\.stripe\.com/);
+	});
+
 	test('it should not show other options when user is only member', async ({ page }) => {
 		await page.goto('/dashboard');
 		expect(page.url()).toContain(`/dashboard/members/${testData.userId}`);
@@ -47,7 +54,7 @@ test.describe('Member Management - Admin', () => {
 	test.beforeEach(async ({ context }) => {
 		await loginAsUser(context, 'admin@test.com');
 	});
-	test.afterAll(() => Promise.all([adminData?.cleanUp(), memberData?.cleanUp()] ));
+	test.afterAll(() => Promise.all([adminData?.cleanUp(), memberData?.cleanUp()]));
 
 	test('it should show other options when user is admin', async ({ page }) => {
 		await page.goto('/dashboard');
@@ -58,7 +65,7 @@ test.describe('Member Management - Admin', () => {
 
 	test('admin should be able to navigate to a specific member profile', async ({ page }) => {
 		await page.goto(`/dashboard/members/${memberData.userId}`);
-		await page.getByLabel(/first name/i).fill('Updated name');	
+		await page.getByLabel(/first name/i).fill('Updated name');
 		await page.getByRole('button', { name: /save changes/i }).click();
 		await expect(page.getByText(/profile has been updated/i)).toBeVisible();
 		await page.reload();
@@ -72,14 +79,15 @@ test.describe('Member management - cross member role check', () => {
 	test.beforeAll(async () => {
 		memberOne = await createMember({ email: 'member1@member.com', roles: new Set(['member']) });
 		memberTwo = await createMember({ email: 'member2@member.com', roles: new Set(['member']) });
-	});test.beforeEach(async ({ context }) => {
+	});
+	test.beforeEach(async ({ context }) => {
 		await loginAsUser(context, 'member1@member.com');
 	});
-	test.afterAll(() => Promise.all([memberOne?.cleanUp(), memberTwo?.cleanUp()] ));
+	test.afterAll(() => Promise.all([memberOne?.cleanUp(), memberTwo?.cleanUp()]));
 
-	test('it should not allow to navigate to another member\'s profile', async ({page}) => {
+	test("it should not allow to navigate to another member's profile", async ({ page }) => {
 		const response = await page.goto(`/dashboard/members/${memberTwo.userId}`);
 		expect(response?.status()).toBe(404);
 		await expect(page.getByText(/member not found/i)).toBeVisible();
-	})
-})
+	});
+});
