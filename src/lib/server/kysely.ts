3a07681bb56736affd86a6bff67e4195a21062d0
alessandrojcm <me@alessandrojcm.dev>
2025-03-22 02:24:57 +0000

    fix: fixed claims for kysely

--- src/lib/server/kysely.ts
+++ src/lib/server/kysely.ts
@@ -1,16 +1,28 @@
-import type { KyselyDatabase } from '$lib/types';
-import { Kysely, sql, Transaction } from 'kysely';
 import { env } from '$env/dynamic/private';
-import postgres from 'postgres';
+import type { KyselyDatabase } from '$lib/types';
+import { type Session } from '@supabase/supabase-js';
+import { jwtDecode } from 'jwt-decode';
+import { Kysely, sql, Transaction } from 'kysely';
 import { PostgresJSDialect } from 'kysely-postgres-js';
-import { type User } from '@supabase/supabase-js';
+import postgres from 'postgres';
 
 interface RLSData {
 	/**
 	 * Claims to be set in the transaction
 	 */
-	claims: User;
+	claims: Session;
 }
+
+type SupabaseToken = {
+	iss?: string;
+	sub?: string;
+	aud?: string[] | string;
+	exp?: number;
+	nbf?: number;
+	iat?: number;
+	jti?: string;
+	role?: string;
+};
 
 export const kysely = new Kysely<KyselyDatabase>({
 	dialect: new PostgresJSDialect({
@@ -37,12 +49,19 @@ export async function executeWithRLS<T>(
 	authData: RLSData,
 	callback: (trx: Transaction<KyselyDatabase>) => Promise<T>
 ) {
+	const decoded = jwtDecode(authData.claims.access_token) as SupabaseToken;
 	return await kysely.transaction().execute(async (trx) => {
 		// set transaction level auth variables and run transaction
-		await sql`
-      SET LOCAL request.jwt.claims = ${sql.lit(JSON.stringify(authData.claims))};
-      SET LOCAL ROLE authenticated;
-    `.execute(trx);
+		sql`
+       -- auth.jwt()
+          select set_config('request.jwt.claims', ${sql.lit(
+						JSON.stringify(authData.claims.access_token)
+					)}, TRUE);
+          -- auth.uid()
+          select set_config('request.jwt.claim.sub', ${sql.lit(decoded.sub ?? '')}, TRUE);												
+          -- set local role
+          set local role ${sql.raw(decoded.role ?? 'anon')};
+          `.execute(trx);
 		return await callback(trx);
 	});
 }
