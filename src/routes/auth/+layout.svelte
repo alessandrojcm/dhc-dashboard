322ad294abb160f7d95a8ddf7f1cd10538eddb5a
alessandrojcm <me@alessandrojcm.dev>
2025-04-13 14:42:14 +0100

    perf: some perf optimization

--- src/routes/auth/+layout.svelte
+++ src/routes/auth/+layout.svelte
@@ -1,5 +1,5 @@
 <script lang="ts">
-    import DHCLogo from '$assets/dhc-logo.svg?raw';
+	import DHCLogo from '$lib/components/dhc-logo.svelte';
 	let { children } = $props();
 </script>
 
@@ -9,7 +9,7 @@
 
 <div class="flex h-[100vh]">
 	<div class="hidden md:flex flex-1 basis-1/4 bg-blue-700 flex-row items-center">
-		{@html DHCLogo}
+		<DHCLogo />
 	</div>
 	<div class="flex-1 flex items-center justify-center basis-3/4">
 		{@render children()}
