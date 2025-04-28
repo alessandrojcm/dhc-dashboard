59e204a4d4bfe6815912045281b901950fab8f55
alessandrojcm <me@alessandrojcm.dev>
2025-04-28 22:58:08 +0100

    fix: migrations diversion

---
+++ supabase/migrations/20250428215230_remote_schema.sql
@@ -0,0 +1,7 @@
+drop policy "Committee members can delete settings" on "public"."settings";
+
+drop policy "Committee members can insert settings" on "public"."settings";
+
+drop policy "Committee members can modify settings" on "public"."settings";
+
+
