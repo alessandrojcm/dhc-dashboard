d5cb40becb7aade670b5b8b6b75568e49a5046ad
alessandrojcm <me@alessandrojcm.dev>
2025-12-17 11:53:28 +0000

    feat: migrated auth and waitlist form to svelte form action

--- src/routes/(public)/waitlist/+page.server.ts
+++ src/routes/(public)/waitlist/+page.server.ts
@@ -1,9 +1,6 @@
-import { error, fail } from '@sveltejs/kit';
-import { message, superValidate } from 'sveltekit-superforms';
-import { valibot } from 'sveltekit-superforms/adapters';
-import { createWaitlistService, WaitlistEntrySchema } from '$lib/server/services/members';
+import { error } from '@sveltejs/kit';
 import { supabaseServiceClient } from '$lib/server/supabaseServiceClient';
-import type { Actions, PageServerLoad } from './$types';
+import type { PageServerLoad } from './$types';
 
 export const load: PageServerLoad = async () => {
 	const isWaitlistOpen = await supabaseServiceClient
@@ -13,39 +10,14 @@ export const load: PageServerLoad = async () => {
 		.single()
 		.throwOnError()
 		.then((result) => result?.data?.value === 'true');
+
 	if (!isWaitlistOpen) {
 		error(401, 'The waitlist is currently closed, please come back later.');
 	}
+
 	return {
-		form: await superValidate(valibot(WaitlistEntrySchema)),
 		genders: await supabaseServiceClient
 			.rpc('get_gender_options')
 			.then((res) => (res.data ?? []) as string[])
 	};
-};
-
-export const actions: Actions = {
-	default: async (event) => {
-		const form = await superValidate(event, valibot(WaitlistEntrySchema));
-		if (!form.valid) {
-			return fail(422, {
-				form
-			});
-		}
-
-		try {
-			const waitlistService = createWaitlistService(event.platform!);
-			await waitlistService.create(form.data);
-		} catch (err) {
-			console.error(err);
-			return message(
-				form,
-				{ error: 'Something has gone wrong, please try again later.' },
-				{ status: 500 }
-			);
-		}
-		return message(form, {
-			success: 'You have been added to the waitlist, we will be in contact soon!'
-		});
-	}
 };
