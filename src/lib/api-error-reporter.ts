f46c3b92e1c846f6d21b669b60a4980b0d00a260
alessandrojcm <me@alessandrojcm.dev>
2026-07-14 14:03:45 +0100

    centrailze api error reporting

---
+++ src/lib/api-error-reporter.ts
@@ -0,0 +1,31 @@
+import { getClient } from "@dhc/api-client";
+import * as Sentry from "@sentry/sveltekit";
+
+let apiErrorReporterRegistered = false;
+
+export function registerApiErrorReporter() {
+	if (apiErrorReporterRegistered) {
+		return;
+	}
+
+	getClient().interceptors.error.use((error, response, request, options) => {
+		Sentry.captureException(error, {
+			tags: {
+				api_client: "hey-api",
+				api_error_name: error instanceof Error ? error.name : typeof error,
+			},
+			contexts: {
+				api: {
+					method: request?.method ?? options.method,
+					url: request?.url ?? options.url,
+					status: response?.status,
+					statusText: response?.statusText,
+				},
+			},
+		});
+
+		return error;
+	});
+
+	apiErrorReporterRegistered = true;
+}
